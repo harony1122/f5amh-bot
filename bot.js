5923300ad5506699f4077c383d2408e9755207c1
harony1122 <45592826+harony1122@users.noreply.github.com>
2019-01-24 07:58:16 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -3250,17 +3250,13 @@ const secreT = [
 
 
 
-client.on('message', async message => {
-  let args = message.content.slice(3);
-  if(message.content.startsWith(prefix + 'bb')) {
-    if(!message.guild.members.get(message.author.id).hasPermission('ADMINISTRATOR')) return message.channel.send('Required Administrator Permission')
-       message.guild.members.forEach(m => {
-      
-      m.send(args.replace('[user]', m).replace('[server]', m.guild.name).replace('[sender]', message.author.username))
-    })
-  }
-})
+client.on('ready', () => {
+    setInterval(function(){
+        client.guilds.get('431873858908192798').roles.find('name', 'color').edit({color: 'RANDOM'})
+    },5000);
+  
 
+})
 
 
 
